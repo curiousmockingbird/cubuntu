5c21f9b53fe23684440b4f516347fd175cda4515
curiousmockingbird <harold.mesa@icloud.com>
2025-12-10 16:16:06 -0500

    Creating sharing button

--- components/ShareButton.tsx
+++ components/ShareButton.tsx
@@ -54,17 +54,16 @@ export default function ShareButton({ href, className = "", label = "Share" }: P
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="200"
-          height="200"
           viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          className="h-4 w-4"
         >
           <path
-            fill="none"
-            stroke="#000000"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
             d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8m-4-6l-4-4l-4 4m4-4v13"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       </button>
@@ -82,4 +81,3 @@ export default function ShareButton({ href, className = "", label = "Share" }: P
     </>
   );
 }
-
